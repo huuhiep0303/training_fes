8b26a404d4474ad3ba536daea9fd3c3048978ad6
HiepGit <cacaohh04@gmail.com>
2025-01-04 11:52:14 +0700

    new commit

---
+++ Lesson8_02_01_25/function.js
@@ -0,0 +1,43 @@
+b='A13'
+c=24
+console.log(b+c)
+
+console.error("123")
+console.warn("123")
+
+console.log(b*c)
+console.log(b/c)
+console.log(b-c)
+
+console.log(b-c)
+
+a={
+    name: 'Hiep',
+    age: 22,
+    address: {
+        street: '123 Main St',
+        city: 'New York',
+        state: 'NY'
+    },
+    'skills': 'JS'
+}
+
+console.log(a.name)
+a.name='ABC'
+console.log(a.name)
+a['name']='Hello'
+console.log(a.name)
+a['skills']='C++'
+console.log(a.skills)
+x='skills'
+a[x]='Html'
+console.log(a[x])
+
+console.log(2==='2')
+console.log(21>'20')
+
+let q=10
+console.log(q++)
+console.log(q)
+console.log(++q)
+
